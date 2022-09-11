8dc46190e54d62a647b6ce90c5d4ed7630e05176
Дмитрий Бунецкий <bunetskydn@gmail.com>
2022-09-11 19:18:00 +0700

    feat: change file extensions js to ts and refactor the components by inheriting from the Block class

---
+++ src/modules/UiKit/Button/Button.ts
@@ -0,0 +1,22 @@
+import template from './Button.hbs';
+import Block from '../../../helpers/block';
+import * as styles from './Button.scss';
+
+interface IButton {
+  label: string;
+  class: string;
+  href?: string;
+  events?: {
+    click?: (event: Event) => void;
+  };
+}
+
+export class Button extends Block {
+  constructor(properties: IButton) {
+    super(properties);
+  }
+
+  render() {
+    return this.compile(template, { ...this.props, styles });
+  }
+}
