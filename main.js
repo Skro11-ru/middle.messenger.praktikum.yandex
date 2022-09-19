001840ee9b7aa85cd70f7679fd2f6048114c6b49
Дмитрий Бунецкий <bunetskydn@gmail.com>
2022-09-20 00:20:13 +0700

    fix: fix folder name

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import renderDom from './src/helpers/renderDOM';
 import { Login } from './src/modules/Login/view/Login';
 import { Registration } from './src/modules/Registration/view/Registration';
-import { Profile } from './src/modules/profile/view/Profile/Profile';
-import { ProfileEdit } from './src/modules/profile/view/ProfileEdit/ProfileEdit';
+import { Profile } from './src/modules/Profile/view/Profile/Profile';
+import { ProfileEdit } from './src/modules/Profile/view/ProfileEdit/ProfileEdit';
 import { Error500 } from './src/modules/UiKit/plug/500/500';
 import { Error404 } from './src/modules/UiKit/plug/404/404';
 import { ChatsPage } from './src/modules/Chat/view/Chat';
